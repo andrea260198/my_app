dcb31e337c8ab513f340893ef544c07c965af368
Andrea Cassotti <cassottiandrea@hotmail.com>
2024-02-24 19:34:43 +0100

    Objects allocated in the stack instead of heap

--- src/main.cpp
+++ src/main.cpp
@@ -7,9 +7,9 @@ int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
 
-    new SystemTrayIcon();
+    SystemTrayIcon SystemTrayIcon;
 
-    new Scheduler();
+    Scheduler scheduler;
 
     return app.exec();
 }
